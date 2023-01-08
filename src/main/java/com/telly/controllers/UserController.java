fb0282942edd9d4e4b7cc38fdcfa9459bd4a66fa
sofian <js_chelihi@esi.dz>
2023-01-08 22:08:58 +0100

    affichage des reservations

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -46,6 +46,23 @@ public class UserController {
 		return "home";
 
 	}
+
+
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+
+
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
+
+
+		return "home";
+
+	}
 	
 
 }
